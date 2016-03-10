6cb4236ddcf67f80306b715553ca0d839e419239
Joshua Harms <joshua@nozzlegear.com>
2016-03-10 15:28:46 -0600

    Messing with ambient Typings defs

--- reqwest.d.ts
+++ reqwest.d.ts
@@ -1,47 +1,44 @@
-declare module reqwest
+interface ReqwestOptions
 {
-    export interface ReqwestOptions
-    {
-        url: string;
-        method?: string;
-        headers?: Object;
-        data?: string | Object;
-        type?: string;
-        contentType?: string;
-        crossOrigin?: boolean;
-        success?: (response: any) => void;
-        error?: (error: any) => void;
-        complete?: (response: any) => void;
-        jsonpCallback?: string;
-    }
-
-    export interface ReqwestPromise<T>
-    {
-        request: XMLHttpRequest;
-
-        then(callback: (response: T) => void): ReqwestPromise<T>;
-
-        fail(callback: (error: XMLHttpRequest, message: string) => void): ReqwestPromise<T>;
-
-        always(callback: (data: any) => void): void;
-    }
-
-    export interface ReqwestStatic
-    {
-        (url: string): ReqwestPromise<any>;
-
-        (url: string, done: (response: any) => void): ReqwestPromise<any>;
-
-        (options: ReqwestOptions): ReqwestPromise<any>;
-
-        <T>(url: string): ReqwestPromise<T>;
-
-        <T>(url: string, done: (response: T) => void): ReqwestPromise<T>;
-
-        <T>(options: ReqwestOptions): ReqwestPromise<T>;
-    }
+    url: string;
+    method?: string;
+    headers?: Object;
+    data?: string | Object;
+    type?: string;
+    contentType?: string;
+    crossOrigin?: boolean;
+    success?: (response: any) => void;
+    error?: (error: any) => void;
+    complete?: (response: any) => void;
+    jsonpCallback?: string;
 }
 
-declare var req: reqwest.ReqwestStatic;
+interface ReqwestPromise<T>
+{
+    request: XMLHttpRequest;
+
+    then(callback: (response: T) => void): ReqwestPromise<T>;
+
+    fail(callback: (error: XMLHttpRequest, message: string) => void): ReqwestPromise<T>;
+
+    always(callback: (data: any) => void): void;
+}
+
+interface ReqwestStatic
+{
+    (url: string): ReqwestPromise<any>;
+
+    (url: string, done: (response: any) => void): ReqwestPromise<any>;
+
+    (options: ReqwestOptions): ReqwestPromise<any>;
+
+    <T>(url: string): ReqwestPromise<T>;
+
+    <T>(url: string, done: (response: T) => void): ReqwestPromise<T>;
+
+    <T>(options: ReqwestOptions): ReqwestPromise<T>;
+}
+
+declare var req: ReqwestStatic;
 
 export = req;
